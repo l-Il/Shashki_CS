54e9cadc15cd4f75091aa77b286a4f502b3fa1d8
l-Il <0rky@pm.me>
2021-04-14 17:24:11 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Shashki_CS
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.player = new System.Windows.Forms.Label();
+      this.timer1 = new System.Windows.Forms.Timer(this.components);
+      this.obyect = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // player
+      // 
+      this.player.Image = ((System.Drawing.Image) (resources.GetObject("player.Image")));
+      this.player.Location = new System.Drawing.Point(130, 450);
+      this.player.Name = "player";
+      this.player.Size = new System.Drawing.Size(99, 96);
+      this.player.TabIndex = 0;
+      this.player.Click += new System.EventHandler(this.player_Click);
+      // 
+      // timer1
+      // 
+      this.timer1.Interval = 50;
+      this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+      // 
+      // obyect
+      // 
+      this.obyect.Image = ((System.Drawing.Image) (resources.GetObject("obyect.Image")));
+      this.obyect.Location = new System.Drawing.Point(130, 10);
+      this.obyect.Name = "obyect";
+      this.obyect.Size = new System.Drawing.Size(99, 96);
+      this.obyect.TabIndex = 2;
+      this.obyect.Click += new System.EventHandler(this.obyect_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackgroundImage = ((System.Drawing.Image) (resources.GetObject("$this.BackgroundImage")));
+      this.ClientSize = new System.Drawing.Size(361, 598);
+      this.Controls.Add(this.obyect);
+      this.Controls.Add(this.player);
+      this.Name = "Form1";
+      this.Text = "Игра";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.Click += new System.EventHandler(this.Form1_Click);
+      this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+      this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+      this.ResumeLayout(false);
+    }
+
+        #endregion
+
+        private System.Windows.Forms.Label player;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label obyect;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shashki_CS
+{
+  public partial class Form1 : Form
+  {
+    public int player_position;
+    public int object_rad;
+    double object_y;
+    public Random rnd = new Random();
+    public int velocity = 150;
+    double time = 0;
+    private bool status = true;
+    public Form1()
+    {
+      InitializeComponent();
+
+      object_y = obyect.Location.Y;
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+      time += timer1.Interval / 1000.0;
+      if (time > 1/2.0 )
+        switch(key)
+        {
+          case Keys.A:
+          case Keys.Left:
+            if (player_position == 1)
+            {
+              player_position = 0;
+            }
+            else if (player_position == 2)
+            {
+              player_position = 1;
+            }
+            else
+            {
+              break;
+            }
+            player.Location = new Point(25 + 105 * player_position, 450);
+            break;
+
+          case Keys.D:
+          case Keys.Right:
+            if (player_position == 1)
+            {
+              player_position = 2;
+            }
+            else if (player_position == 0)
+            {
+              player_position = 1;
+            }
+            else
+            {
+              break;
+            }
+
+            player.Location = new Point(25 + 105 * player_position, 450);
+            break;
+        }
+      obyect.Text = object_rad.ToString();
+      player.Text = player_position.ToString();
+      if (time > 50) time -= 50;
+      object_y += velocity * timer1.Interval / 1000.0;
+      Point op = obyect.Location;
+      op.Y = (int)object_y;
+      obyect.Location = op;
+      if (op.Y > 550)
+      {
+        object_rad = rnd.Next(0, 3);
+        obyect.Location = new Point(25 + 105 * object_rad, 10);
+        object_y = 10;
+      }
+      
+      if (player_position == object_rad && obyect.Location.Y >= 354 && obyect.Location.Y < 550)
+      {
+        if (status == true)
+        {
+          MessageBox.Show("Мать умерла!");
+          status = false;
+        }
+        
+      }
+      
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      timer1.Enabled = true;
+      player_position = 1;
+      object_rad = rnd.Next(0, 3);
+      obyect.Location = new Point(25 + 105 * (int)object_rad, 10);
+    }
+    Keys key;
+    private void Form1_KeyDown(object sender, KeyEventArgs e)
+    {
+      key = e.KeyCode;
+    }
+    private void Form1_KeyUp(object sender, KeyEventArgs e)
+    {
+      key = Keys.None;
+    }
+    private void Form1_Click(object sender, EventArgs e){}
+    private void obyect_Click(object sender, EventArgs e){}
+    private void player_Click(object sender, EventArgs e){}
+  }
+}
